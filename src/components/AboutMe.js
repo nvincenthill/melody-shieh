b574ca06e8e804b08c97648a8ffcd149f53e6d93
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-12 19:03:30 -0700

    Reposition aboutme items and add paragraph text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -39,6 +39,9 @@ class AboutMe extends React.Component {
               strategy, and operational improvement.
             </p>
             <p className="about-description-text">
+            Recently at A&M, Melody supported a $60B national healthcare provider and insurer with an IT transformation from an old-world infrastructure delivery process to an outsourced “as-a-service” model. She identified a net savings opportunity of $472.1M (~35%) by determining cost drivers and duplication between capital, vendor, and staffing spend within the client's service delivery model. The results refined the client’s view of their core services and streamlined their service delivery approach across the customer value chain.
+            </p>
+            <p className="about-description-text">
               Previously, Melody conducted an objective discovery assessment for
               a leading national healthcare provider's outpatient scheduling
               function and delivered recommendations for a future, consolidated
